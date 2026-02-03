235475685c7d800a222536248252e9339c4ad2dc
Waters <h2oboi89@gmail.com>
2026-02-03 17:49:04 -0500

    Added bench test output to README

--- BenchTest/Program.cs
+++ BenchTest/Program.cs
@@ -1,114 +1,117 @@
 ﻿using StableIndexCollection;
 using System.Diagnostics;
 
-const int ITEM_COUNT = 10_000_000;
-const int REMOVE_COUNT = 1_000;
+int ITEM_COUNT_SEED = 1_000;
+int REMOVE_COUNT_SEED = 10;
 
-var intItems = new int[ITEM_COUNT];
-var callableItems = new CallableFunction[ITEM_COUNT];
-
-for (var i = 0; i < 1_000_000; i++)
+for (var l = 1; l < 5; l++)
 {
-    intItems[i] = i;
-    callableItems[i] = new CallableFunction();
+    var ITEM_COUNT = ITEM_COUNT_SEED * (int)Math.Pow(10, l);
+    var REMOVE_COUNT = REMOVE_COUNT_SEED * (int)Math.Pow(10, l);
+
+    var intItems = new int[ITEM_COUNT];
+
+    for (var i = 0; i < ITEM_COUNT; i++)
+    {
+        intItems[i] = i;
+    }
+
+    LinkedList<int> linkedListInt = new();
+    List<int> listInt = [];
+    StableIndexCollection<int> sivInt = [];
+
+    int sum = 0;
+
+    Console.WriteLine($"{nameof(ITEM_COUNT)}: {ITEM_COUNT}, {nameof(REMOVE_COUNT)}: {REMOVE_COUNT}");
+    sum = 0;
+    var random = new Random();
+    var seed = random.Next();
+
+    random = new Random(seed);
+    BenchmarkFunction(() => linkedListInt = new LinkedList<int>(intItems), "create LinkedList<int>");
+    BenchmarkFunction(() =>
+    {
+        foreach (var item in linkedListInt)
+        {
+            sum = unchecked(sum + item);
+        }
+    }, "sum LinkedList<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            linkedListInt.Remove(i);
+        }
+    }, "remove LinkedList<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            linkedListInt.AddLast(i);
+        }
+    }, "add LinkedList<int>");
+
+    Console.WriteLine(sum);
+    sum = 0;
+
+    linkedListInt.Clear();
+
+    random = new Random(seed);
+    BenchmarkFunction(() => listInt = [.. intItems], "create List<int>");
+    BenchmarkFunction(() =>
+    {
+        foreach (var item in listInt)
+        {
+            sum = unchecked(sum + item);
+        }
+    }, "sum List<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            listInt.Remove(i);
+        }
+    }, "remove List<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            listInt.Add(i);
+        }
+    }, "add List<int>");
+
+    listInt.Clear();
+
+    Console.WriteLine(sum);
+    sum = 0;
+
+    random = new Random(seed);
+    BenchmarkFunction(() => sivInt = [.. intItems], "create StableIndexCollection<int>");
+    BenchmarkFunction(() =>
+    {
+        foreach (var item in sivInt)
+        {
+            sum = unchecked(sum + item);
+        }
+    }, "sum StableIndexCollection<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            sivInt.Remove(i);
+        }
+    }, "remove StableIndexCollection<int>");
+    BenchmarkFunction(() =>
+    {
+        for (var i = 0; i < REMOVE_COUNT; i++)
+        {
+            sivInt.Add(i);
+        }
+    }, "add StableIndexCollection<int>");
+
+    Console.WriteLine(sum);
+    sum = 0;
 }
-
-LinkedList<int> linkedListInt = new();
-List<int> listInt = [];
-StableIndexCollection<int> sivInt = [];
-
-int sum = 0;
-
-Console.WriteLine(sum);
-sum = 0;
-var random = new Random();
-var seed = random.Next();
-
-
-random = new Random(seed);
-BenchmarkFunction(() => linkedListInt = new LinkedList<int>(intItems), "create LinkedList<int>");
-BenchmarkFunction(() =>
-{
-    foreach(var item in linkedListInt)
-    {
-        sum = unchecked(sum + item);
-    }
-}, "sum LinkedList<int>");
-BenchmarkFunction(() =>
-{
-    for(var i = 0; i < REMOVE_COUNT; i++)
-    {
-        linkedListInt.Remove(i);
-    }
-}, "remove LinkedList<int>");
-BenchmarkFunction(() =>
-{
-    for (var i = 0; i < REMOVE_COUNT; i++)
-    {
-        linkedListInt.AddLast(i);
-    }
-}, "add LinkedList<int>");
-
-Console.WriteLine(sum);
-sum = 0;
-
-linkedListInt.Clear();
-
-random = new Random(seed);
-BenchmarkFunction(() => listInt = [.. intItems], "create List<int>");
-BenchmarkFunction(() =>
-{
-    foreach(var item in listInt)
-    {
-        sum = unchecked(sum + item);
-    }
-}, "sum List<int>");
-BenchmarkFunction(() =>
-{
-    for (var i = 0; i < REMOVE_COUNT; i++)
-    {
-        listInt.Remove(i);
-    }
-}, "remove List<int>");
-BenchmarkFunction(() =>
-{
-    for (var i = 0; i < REMOVE_COUNT; i++)
-    {
-        listInt.Add(i);
-    }
-}, "add List<int>");
-
-listInt.Clear();
-
-Console.WriteLine(sum);
-sum = 0;
-
-random = new Random(seed);
-BenchmarkFunction(() => sivInt = [.. intItems], "create StableIndexCollection<int>");
-BenchmarkFunction(() =>
-{
-    foreach(var item in sivInt)
-    {
-        sum = unchecked(sum + item);
-    }
-}, "sum StableIndexCollection<int>");
-BenchmarkFunction(() =>
-{
-    for (var i = 0; i < REMOVE_COUNT; i++)
-    {
-        sivInt.Remove(i);
-    }
-}, "remove StableIndexCollection<int>");
-BenchmarkFunction(() =>
-{
-    for (var i = 0; i < REMOVE_COUNT; i++)
-    {
-        sivInt.Add(i);
-    }
-}, "add StableIndexCollection<int>");
-
-Console.WriteLine(sum);
-sum = 0;
 
 static void BenchmarkFunction(Action action, string name)
 {
@@ -116,14 +119,4 @@ static void BenchmarkFunction(Action action, string name)
     action();
     stopWatch.Stop();
     Console.WriteLine($"{name}: {stopWatch.ElapsedMilliseconds} ms");
-}
-
-class CallableFunction
-{
-    private int _value = 0;
-
-    public void Update()
-    {
-        _value++;
-    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-C# Implementation of https://github.com/johnBuffer/StableIndexVector
+# C# Implementation of https://github.com/johnBuffer/StableIndexVector
+
+## Bench Test Output
+```
+> .\BenchTest.exe
+ITEM_COUNT: 10000, REMOVE_COUNT: 100
+create LinkedList<int>: 1 ms
+sum LinkedList<int>: 0 ms
+remove LinkedList<int>: 0 ms
+add LinkedList<int>: 0 ms
+49995000
+create List<int>: 0 ms
+sum List<int>: 0 ms
+remove List<int>: 0 ms
+add List<int>: 0 ms
+49995000
+create StableIndexCollection<int>: 2 ms
+sum StableIndexCollection<int>: 0 ms
+remove StableIndexCollection<int>: 0 ms
+add StableIndexCollection<int>: 0 ms
+49995000
+ITEM_COUNT: 100000, REMOVE_COUNT: 1000
+create LinkedList<int>: 2 ms
+sum LinkedList<int>: 0 ms
+remove LinkedList<int>: 0 ms
+add LinkedList<int>: 0 ms
+704982704
+create List<int>: 0 ms
+sum List<int>: 0 ms
+remove List<int>: 4 ms
+add List<int>: 0 ms
+704982704
+create StableIndexCollection<int>: 11 ms
+sum StableIndexCollection<int>: 0 ms
+remove StableIndexCollection<int>: 0 ms
+add StableIndexCollection<int>: 0 ms
+704982704
+ITEM_COUNT: 1000000, REMOVE_COUNT: 10000
+create LinkedList<int>: 72 ms
+sum LinkedList<int>: 3 ms
+remove LinkedList<int>: 0 ms
+add LinkedList<int>: 0 ms
+1783293664
+create List<int>: 0 ms
+sum List<int>: 0 ms
+remove List<int>: 3922 ms
+add List<int>: 0 ms
+1783293664
+create StableIndexCollection<int>: 201 ms
+sum StableIndexCollection<int>: 1 ms
+remove StableIndexCollection<int>: 6 ms
+add StableIndexCollection<int>: 1 ms
+1783293664
+ITEM_COUNT: 10000000, REMOVE_COUNT: 100000
+create LinkedList<int>: 817 ms
+sum LinkedList<int>: 28 ms
+remove LinkedList<int>: 0 ms
+add LinkedList<int>: 2 ms
+-2014260032
+create List<int>: 6 ms
+sum List<int>: 5 ms
+remove List<int>: 483146 ms
+add List<int>: 0 ms
+-2014260032
+create StableIndexCollection<int>: 2792 ms
+sum StableIndexCollection<int>: 14 ms
+remove StableIndexCollection<int>: 403 ms
+add StableIndexCollection<int>: 1 ms
+-2014260032
+```
